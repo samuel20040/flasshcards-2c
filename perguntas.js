8fb91f133fb9d42088b487ccf674cb16ae68e5ee
samuel20040 <samuel.delgado.ribeiro@escola.pr.gov.br>
2025-05-12 14:45:51 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    programação,
+    O que javaScript?,
+    resposta
+)
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+) 
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
